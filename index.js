9416d146890c7fe68345de93dfde795494213df3
didi <adrien.pirmez@gmail.com>
2024-12-18 01:06:25 -0300

    aula 24 google keys

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy 
+
+
+
 const app = express()
 
+passport.use(new GoogleStrategy())
+/*
 app.get('/', (req, res) => {
     res.send({hi:'there'})
 })
-
-app.listen(5000)
+    */
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
